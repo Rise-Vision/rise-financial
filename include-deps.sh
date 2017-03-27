62a07b4b149ce99243799343d6a6d8501ec28057
Stuart Lees <stuartlees@gmail.com>
2017-03-27 15:19:11 -0400

    Include local version of firebase js source

---
+++ include-deps.sh
@@ -0,0 +1,4 @@
+mkdir -p firebasejs/3.6.1
+curl "https://www.gstatic.com/firebasejs/3.6.1/firebase-app.js" -o firebasejs/3.6.1/firebase-app.js
+
+curl "https://www.gstatic.com/firebasejs/3.6.1/firebase-database.js" -o firebasejs/3.6.1/firebase-database.js
