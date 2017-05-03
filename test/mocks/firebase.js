a786928bd19eac4aaa8bf391b502152df21c37c5
Xiyang Chen <settinghead@gmail.com>
2017-05-03 05:20:49 -0400

    Add integration test for offline localStorage test case

--- test/mocks/firebase.js
+++ test/mocks/firebase.js
@@ -1,33 +1,61 @@
-/* exported firebase */
-var firebase = {
-  initializeApp: ( config ) => {
-    return config;
-  },
-  apps: [],
-  database: () => {
-    return {
-      ref: () => {
-        return {
-          on: ( key, cb ) => {
-            if ( key !== "value" ) {
-              return;
-            }
+/* exported firebase setFirebaseConnectionStatus */
 
-            cb( {
-              val: () => {
-                return {
-                  "AA?N": {
-                    category: "Stocks",
-                    index: 0,
-                    name: "Alcoa",
-                    symbol: "AA.N"
-                  }
+var _firebaseConnectionStatus = true,
+  fireConnStatusCallback,
+  firebase = {
+    initializeApp: ( config ) => {
+      return config;
+    },
+    apps: [],
+    database: () => {
+      return {
+        ref: ( path ) => {
+          if ( path === ".info/connected" ) {
+            return {
+              on: ( key, cb ) => {
+                if ( key !== "value" ) {
+                  return;
                 }
+                fireConnStatusCallback = cb;
+                fireConn();
               }
-            } );
+            };
+          } else {
+            return {
+              on: ( key, cb ) => {
+                if ( key !== "value" ) {
+                  return;
+                }
+
+                cb( {
+                  val: () => {
+                    return {
+                      "AA?N": {
+                        category: "Stocks",
+                        index: 0,
+                        name: "Alcoa",
+                        symbol: "AA.N"
+                      }
+                    }
+                  }
+                } );
+              }
+            };
           }
         }
-      }
-    };
-  }
-};
+      };
+    }
+  };
+
+function fireConn() {
+  fireConnStatusCallback( {
+    val: () => {
+      return _firebaseConnectionStatus;
+    }
+  } )
+}
+
+function setFirebaseConnectionStatus( status ) {
+  _firebaseConnectionStatus = status;
+  fireConn();
+}
