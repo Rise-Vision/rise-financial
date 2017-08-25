5ede234783c63ca520c9aa9572b1c004c74cfb84
Stuart Lees <stuartlees@gmail.com>
2017-08-25 15:51:43 -0400

    Ensure _firebaseConnected value not set until debouncer completes

--- rise-financial-es6.js
+++ rise-financial-es6.js
@@ -419,18 +419,18 @@
     }
 
     _handleConnected( snapshot ) {
-      this._firebaseConnected = snapshot.val();
-
       if ( !this._instrumentsReceived ) {
-        if ( !this._firebaseConnected ) {
+        if ( !snapshot.val() ) {
           // account for multiple "false" values being initially returned even though network status is online
           if ( !this.isDebouncerActive( "connected" ) ) {
             this.debounce( "connected", () => {
+              this._firebaseConnected = false;
               this._getInstruments();
             }, 2000 );
           }
         } else {
           this.cancelDebouncer( "connected" );
+          this._firebaseConnected = true;
           this._getInstruments();
         }
       }
